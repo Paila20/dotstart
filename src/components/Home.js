8fc06e15fa7cb346301527763abeb09a5bdf49ad
paila20 <rajarajeswar169@gmail.com>
2024-10-22 15:27:25 +0530

    added carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,9 @@ import React from 'react';
 import { imagearray1 } from '../Constants'; 
 import { IconArray } from '../Constants';
 import { FaMapMarkerAlt } from 'react-icons/fa'; 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import Link from "../assets/Link.png";
 import Link1 from "../assets/Link 1.png";
 import Link2 from "../assets/Link 2.png";
@@ -23,10 +26,36 @@ import image13 from "../assets/image13.png";
 import image14 from "../assets/image14.png";
 import image15 from "../assets/image15.png";
 import { FaBed, FaBath, FaRulerCombined } from 'react-icons/fa';
-import { FaArrowRight } from 'react-icons/fa';
+import { FaArrowRight,FaChevronLeft,FaChevronRight } from 'react-icons/fa';
 import "./Home.css";
 
 const App = () => {
+
+  const settings1 = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+   
+   
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
   return (
     <>
     <div className="container">
@@ -34,7 +63,8 @@ const App = () => {
             <h1>Homes For You</h1>
             <p>Based on your view history</p>
         </div>
-      <div className="main-card">
+  
+      <Slider {...settings1} className="main-card">
         {imagearray1.map((card, index) => (
           <div className="card" key={index}>
             <img src={card.src} alt={card.title} className="card-image" />
@@ -55,7 +85,8 @@ const App = () => {
             </div>
           </div>
         ))}
-      </div>
+        </Slider>
+    
     </div>
     <div className='container2'>
 
